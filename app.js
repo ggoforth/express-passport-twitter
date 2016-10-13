836bc148d8e683637f2664ebf4d0818e99211b08
Greg Goforth <greg.goforth@gmail.com>
2016-10-12 18:52:17 -0700

    login with twitter

--- app.js
+++ app.js
@@ -6,12 +6,22 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('passport');
 var TwitterStrategy = require('passport-twitter');
+var session = require('express-session');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+//configure sessions
+//session config
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {secure: false}
+}));
 
 //configure passport
 app.use(passport.initialize());
@@ -30,6 +40,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/auth/twitter', passport.authenticate('twitter'));
+app.get('/auth/twitter/callback', passport.authenticate('twitter', {
+  failureRedirect: '/',
+  session: false
+}), (req, res, next) => {
+  //they authorized
+  res.json(req.user);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
